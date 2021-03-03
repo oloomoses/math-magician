53d113202fb11dbb4069a9a89e94729816ce0af6
oloomoses <olooine@gmail.com>
2021-03-04 02:07:16 +0300

    add components directory create Button Component create ButtonPanel Component Add Display component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+  return (
+    <div>
+      <h1>Buttons</h1>
+    </div>
+  );
+}
+
+export default Button;
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ButtonPanel() {
+  return (
+    <div>
+      <h1>Button Panel</h1>
+    </div>
+  );
+}
+
+export default ButtonPanel;
