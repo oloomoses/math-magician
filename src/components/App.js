a2b21f7f08b624b7bb33329fcb1e6a5e0984db34
oloomoses <olooine@gmail.com>
2021-03-08 20:36:09 +0300

    add calculate in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
 function App() {
   return (
     <>
-      <Display />
+      <Display result={calculate} />
       <ButtonPanel />
     </>
   );
