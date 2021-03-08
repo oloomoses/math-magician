56fa75832ee293a3bfc4c66f91a3d652d65520bd
oloomoses <olooine@gmail.com>
2021-03-09 02:46:23 +0300

    update App.js to class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display result={calculate} />
-      <ButtonPanel />
-    </>
-  );
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(btnName) {
+    calculate(this.state, btnName);
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <>
+        <Display result={`${total} ${operation} ${next}`} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </>
+    );
+  }
 }
 
 export default App;
