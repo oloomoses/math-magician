4960b17c0e21f650b195e3c9cf367a56fec10892
oloomoses <olooine@gmail.com>
2021-03-16 23:13:03 +0300

    create Home component create Navbar component create quote component create routes add styling for the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
+import Navbar from './Navbar';
+import Home from './Home';
+import Quote from './Quote';
 import '../App.css';
 
 const App = () => {
@@ -12,12 +16,25 @@ const App = () => {
   };
 
   return (
-    <>
-      <div className="calculator">
-        <Display result={`${state.total} ${state.operation} ${state.next}`} />
-        <ButtonPanel clickHandler={handleClick} />
-      </div>
-    </>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route
+            exact
+            path="/calculator"
+            render={() => (
+              <div className="calculator">
+                <Display result={`${state.total} ${state.operation} ${state.next}`} />
+                <ButtonPanel clickHandler={handleClick} />
+              </div>
+            )}
+          />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/quote" component={Quote} />
+        </Switch>
+      </>
+    </Router>
   );
 };
 
